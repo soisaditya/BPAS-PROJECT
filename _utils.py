c1647121f3e9c79074ca532caffe64b954488a9d
Aditya Srivastava <srivastavaayush701@gmail.com>
2026-05-01 21:48:40 +0530

    Add files via upload

--- _utils.py
+++ _utils.py
@@ -1,36 +1,21 @@
-from __future__ import annotations
-
-import enum
-import typing as t
+import warnings
 
 
-class Sentinel(enum.Enum):
-    """Enum used to define sentinel values.
-
-    .. seealso::
-
-        `PEP 661 - Sentinel Values <https://peps.python.org/pep-0661/>`_.
-    """
-
-    UNSET = object()
-    FLAG_NEEDS_VALUE = object()
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}.{self.name}"
-
-
-UNSET = Sentinel.UNSET
-"""Sentinel used to indicate that a value is not set."""
-
-FLAG_NEEDS_VALUE = Sentinel.FLAG_NEEDS_VALUE
-"""Sentinel used to indicate an option was passed as a flag without a
-value but is not a flag option.
-
-``Option.consume_value`` uses this to prompt or use the ``flag_value``.
-"""
-
-T_UNSET = t.Literal[UNSET]  # type: ignore[valid-type]
-"""Type hint for the :data:`UNSET` sentinel value."""
-
-T_FLAG_NEEDS_VALUE = t.Literal[FLAG_NEEDS_VALUE]  # type: ignore[valid-type]
-"""Type hint for the :data:`FLAG_NEEDS_VALUE` sentinel value."""
+def _raise_warning(attr: str, submodule: str | None = None) -> None:
+    new_module = "numpy._core"
+    old_module = "numpy.core"
+    if submodule is not None:
+        new_module = f"{new_module}.{submodule}"
+        old_module = f"{old_module}.{submodule}"
+    warnings.warn(
+        f"{old_module} is deprecated and has been renamed to {new_module}. "
+        "The numpy._core namespace contains private NumPy internals and its "
+        "use is discouraged, as NumPy internals can change without warning in "
+        "any release. In practice, most real-world usage of numpy.core is to "
+        "access functionality in the public NumPy API. If that is the case, "
+        "use the public NumPy API. If not, you are using NumPy internals. "
+        "If you would still like to access an internal attribute, "
+        f"use {new_module}.{attr}.",
+        DeprecationWarning,
+        stacklevel=3
+    )
