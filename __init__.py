688a8f5e3f622774bff1bcc584fe6b0c59a35203
Aditya Srivastava <srivastavaayush701@gmail.com>
2026-05-01 21:53:13 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
-def f2py_build_generator(name):
-    if name == "meson":
-        from ._meson import MesonBackend
-        return MesonBackend
-    elif name == "distutils":
-        from ._distutils import DistutilsBackend
-        return DistutilsBackend
-    else:
-        raise ValueError(f"Unknown backend: {name}")
+import pytest
+
+from numpy.testing import IS_EDITABLE, IS_WASM
+
+if IS_WASM:
+    pytest.skip(
+        "WASM/Pyodide does not use or support Fortran",
+        allow_module_level=True
+    )
+
+
+if IS_EDITABLE:
+    pytest.skip(
+        "Editable install doesn't support tests with a compile step",
+        allow_module_level=True
+    )
